c71008ecabb41815b2d159dbaef25d98b8dec35e
runv <nvrubinova@gmail.com>
2018-02-16 09:46:14 +0100

    better formatting of html string

--- app/components/html/dialog.html.js
+++ app/components/html/dialog.html.js
@@ -1,7 +1,21 @@
 define(function () {
     return {
         getHTML: function () {
-            return "<div id=\"dialog\"><div id=\"dialogContent\"><div id=\"dialog-header\"><h3 id=\"titleId\"></h3></div><div id=\"dialog-body\"><p id=\"messageId\"></p></div><div id=\"dialog-footer\"><button id=\"okBtn\">OK</button></div></div></div>";
+            return "\
+            <div id=\"dialog\">\
+                <div id=\"dialogContent\">\
+                    <div id=\"dialog-header\">\
+                        <h3 id=\"titleId\"></h3>\
+                    </div>\
+                    <div id=\"dialog-body\">\
+                        <p id=\"messageId\"></p>\
+                    </div>\
+                    <div id=\"dialog-footer\">\
+                        <button id=\"okBtn\">OK</button>\
+                    </div>\
+                </div>\
+            </div>\
+            ";
         }
     };
 });
